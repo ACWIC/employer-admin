e88a670c60d479f73bba96c723baccb9792927a1
Ayesha Siddiqa <ayeshasiddiqatheprogrammer@gmail.com>
2020-10-10 23:02:14 +0500

    Implemented Issue-87_GetEnrolmentStatus

--- app/repositories/s3_enrolment_repo.py
+++ app/repositories/s3_enrolment_repo.py
@@ -1,3 +1,4 @@
+import json
 from typing import Any, Union
 
 import boto3
@@ -5,7 +6,10 @@ import boto3
 from app.config import settings
 from app.domain.entities.enrolment import Enrolment
 from app.repositories.enrolment_repo import EnrolmentRepo
+from app.repositories.s3_callback_repo import S3CallbackRepo
 from app.utils.random import Random
+
+callback_repo = S3CallbackRepo()
 
 
 class S3EnrolmentRepo(EnrolmentRepo):
@@ -32,15 +36,13 @@ class S3EnrolmentRepo(EnrolmentRepo):
         )
 
         self.s3.put_object(
-            Body=bytes(enrl.shared_secret, "utf-8"),
+            Body=bytes(enrl.json(), "utf-8"),
+            # Body=bytes(enrl.shared_secret, "utf-8"),
             Key=f"enrolments/{enrl.enrolment_id}.json",
             Bucket=settings.ENROLMENT_BUCKET,
         )
 
         return enrl
-
-    def get_enrolment(self, enrolment_id: str) -> None:
-        pass
 
     def is_reference_unique(
         self, ref_hash: str
@@ -57,3 +59,30 @@ class S3EnrolmentRepo(EnrolmentRepo):
             return True
         else:
             return False
+
+    def get_enrolment(self, enrolment_id: str):
+        print(
+            "get_enrolment() enrolment_id, BUCKET",
+            enrolment_id,
+            settings.ENROLMENT_BUCKET,
+        )
+        obj = self.s3.get_object(
+            Key=f"enrolments/{enrolment_id}.json", Bucket=settings.ENROLMENT_BUCKET
+        )
+        enrolment = Enrolment(**json.loads(obj["Body"].read().decode()))
+        return enrolment
+
+    def get_enrolment_status(self, enrolment_id: str):
+        callbacks_list = callback_repo.get_callbacks_list(enrolment_id)
+        total_callbacks = len(callbacks_list["callbacks_list"])
+        most_recent_callback = ""
+        for row in callbacks_list["callbacks_list"]:
+            most_recent_callback = row["received"]
+
+        enrolment = {
+            "status": {
+                "total_callbacks": str(total_callbacks),
+                "most_recent_callback": str(most_recent_callback),
+            }
+        }
+        return enrolment
---
+++ app/requests/callback_requests.py
@@ -0,0 +1,8 @@
+from app.requests import ValidRequest
+
+
+class CallbackRequest(ValidRequest):
+    enrolment_id: str
+    key: str
+    tp_sequence: int
+    payload: dict
