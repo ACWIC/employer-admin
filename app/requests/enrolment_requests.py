9c03ebf1c834d2df9787364023aa68674a1f903c
TheAyeshaSiddiqa <ayeshasiddiqatheprogrammer@gmail.com>
2020-11-03 14:35:15 +0500

    Issue-95_PostEnrolment

--- app/requests/enrolment_requests.py
+++ app/requests/enrolment_requests.py
@@ -3,14 +3,3 @@ from app.requests import ValidRequest
 
 class NewEnrolmentRequest(ValidRequest):
     internal_reference: str
-
-
-class PostEnrolmentRequest(ValidRequest):
-    enrolment_id: str
-    course_id: str
-    employee_id: str
-    employee_contact: str
-    agree_pay_fee: bool
-    employee_info_share: bool
-    employer_endpoint: str
-    sender_sequence: str
