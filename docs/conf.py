8d7ada82d31c4bb10eaae102687228b113882822
Chris Gough <christopher.d.gough@gmail.com>
2020-11-09 16:55:31 +1100

    add initial rtd stuff

---
+++ docs/conf.py
@@ -0,0 +1,17 @@
+project = 'ACWIC employer-admin API service'
+copyright = '2020, Aged Care Industry Working Group Pty Ltd'
+author = 'Chris Gough'
+release = '0.0'
+extensions = [
+    'sphinxcontrib.plantuml',
+    'sphinx.ext.graphviz',
+]
+templates_path = ['_templates']
+exclude_patterns = [
+    '_build',
+    'Thumbs.db',
+    '.DS_Store',
+    '.venv'
+]
+html_theme = 'classic'
+html_static_path = ['_static']
