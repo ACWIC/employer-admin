2e3ab150b1370e07dda7be4763ce8baf7d30464a
Chris Gough <christopher.d.gough@gmail.com>
2020-11-09 17:03:42 +1100

    fix conf.py formatting (again)

--- docs/conf.py
+++ docs/conf.py
@@ -1,17 +1,12 @@
-project = 'ACWIC employer-admin API service'
-copyright = '2020, Aged Care Industry Working Group Pty Ltd'
-author = 'Chris Gough'
-release = '0.0'
+project = "ACWIC employer-admin API service"
+copyright = "2020, Aged Care Industry Working Group Pty Ltd"
+author = "Chris Gough"
+release = "0.0"
 extensions = [
-    'sphinxcontrib.plantuml',
-    'sphinx.ext.graphviz',
+    "sphinxcontrib.plantuml",
+    "sphinx.ext.graphviz",
 ]
-templates_path = ['_templates']
-exclude_patterns = [
-    '_build',
-    'Thumbs.db',
-    '.DS_Store',
-    '.venv'
-]
-html_theme = 'classic'
-html_static_path = ['_static']
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store", ".venv"]
+html_theme = "classic"
+html_static_path = ["_static"]
