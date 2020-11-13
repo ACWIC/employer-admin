985c5a6ba1767dcd54ce752092bdae2d5d247b14
Baris Sari <sariba@itu.edu.tr>
2020-11-13 12:04:18 +0300

    Remove unnecessary tests, correct texts

--- tests/api/test_enrolments.py
+++ tests/api/test_enrolments.py
@@ -33,25 +33,6 @@ def test_create_enrolment_success_created(use_case):
 
 
 @mock.patch("app.use_cases.create_enrolment.CreateNewEnrolment")
-def test_create_enrolment_success(use_case):
-    message = "The callback has been fetched from the server."
-    use_case().execute.return_value = ResponseSuccess(
-        value=test_data.sample_enrolment,
-        message=message,
-    )
-
-    data = test_data.sample_enrolment_request
-    response = client.post("/enrolments", data=data.json())
-    json_result = response.json()
-    enrolment = Enrolment(**json_result.get("value"))
-
-    use_case().execute.assert_called_with(data)
-    assert response.status_code == SuccessType.SUCCESS.value
-    assert enrolment == test_data.sample_enrolment
-    assert json_result.get("message") == message
-
-
-@mock.patch("app.use_cases.create_enrolment.CreateNewEnrolment")
 def test_create_enrolment_failure(use_case):
     message = "Error"
     use_case().execute.return_value = ResponseFailure.build_from_resource_error(
@@ -66,29 +47,8 @@ def test_create_enrolment_failure(use_case):
 
 
 @mock.patch("app.use_cases.get_enrolment.GetEnrolmentByID")
-def test_get_enrolment_by_id_success_created(use_case):
-    code = SuccessType.CREATED
-    message = "The enrolment has been created."
-    use_case().execute.return_value = ResponseSuccess(
-        value=test_data.sample_enrolment,
-        type=code,
-        message=message,
-    )
-
-    data = test_data.enrolment_id
-    response = client.get(f"/enrolments/{test_data.enrolment_id}")
-    json_result = response.json()
-    enrolment = Enrolment(**json_result.get("value"))
-
-    use_case().execute.assert_called_with(data)
-    assert response.status_code == SuccessType.CREATED.value
-    assert enrolment == test_data.sample_enrolment
-    assert json_result.get("message") == message
-
-
-@mock.patch("app.use_cases.get_enrolment.GetEnrolmentByID")
 def test_get_enrolment_by_id_success(use_case):
-    message = "The callback has been fetched from the server."
+    message = "The enrolment has been fetched from the server."
     use_case().execute.return_value = ResponseSuccess(
         value=test_data.sample_enrolment,
         message=message,
@@ -119,29 +79,8 @@ def test_get_enrolment_by_id_failure(use_case):
 
 
 @mock.patch("app.use_cases.get_enrolment_status.GetEnrolmentStatus")
-def test_get_enrolment_status_success_created(use_case):
-    code = SuccessType.CREATED
-    message = "The enrolment has been created."
-    use_case().execute.return_value = ResponseSuccess(
-        value=test_data.sample_enrolment,
-        type=code,
-        message=message,
-    )
-
-    data = test_data.enrolment_id
-    response = client.get(f"/enrolments/{test_data.enrolment_id}/status")
-    json_result = response.json()
-    enrolment = Enrolment(**json_result.get("value"))
-
-    use_case().execute.assert_called_with(data)
-    assert response.status_code == SuccessType.CREATED.value
-    assert enrolment == test_data.sample_enrolment
-    assert json_result.get("message") == message
-
-
-@mock.patch("app.use_cases.get_enrolment_status.GetEnrolmentStatus")
 def test_get_enrolment_status_success(use_case):
-    message = "The callback has been fetched from the server."
+    message = "The Enrolment status has been fetched from the server."
     use_case().execute.return_value = ResponseSuccess(
         value=test_data.sample_enrolment,
         message=message,
@@ -172,28 +111,8 @@ def test_get_enrolment_status_failure(use_case):
 
 
 @mock.patch("app.use_cases.get_callbacks_list.GetCallbacksList")
-def test_get_callbacks_list_for_enrolment_success_created(use_case):
-    code = SuccessType.CREATED
-    message = "The enrolment has been created."
-    use_case().execute.return_value = ResponseSuccess(
-        value=test_data.callbacks_list1,
-        type=code,
-        message=message,
-    )
-
-    data = test_data.enrolment_id
-    response = client.get(f"/enrolments/{test_data.enrolment_id}/journal")
-    json_result = response.json()
-
-    use_case().execute.assert_called_with(data)
-    assert response.status_code == SuccessType.CREATED.value
-    assert json_result.get("value") == test_data.callbacks_list1_json
-    assert json_result.get("message") == message
-
-
-@mock.patch("app.use_cases.get_callbacks_list.GetCallbacksList")
 def test_get_callbacks_list_for_enrolment_success(use_case):
-    message = "The callback has been fetched from the server."
+    message = "The Callbacks list has been fetched from the server."
     use_case().execute.return_value = ResponseSuccess(
         value=test_data.callbacks_list1,
         message=message,
